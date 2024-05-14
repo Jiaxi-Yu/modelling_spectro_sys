f39d7bb5063752a4a3535a0371852a4ad37e1f3a
Jiaxi-Yu <yujxjessy@outlook.com>
2024-05-14 06:22:24 +0200

    added tutorial, corrected the systematics implementation

--- spectroscopic_sys_simple.py
+++ spectroscopic_sys_simple.py
@@ -1,4 +1,4 @@
-from numpy import log, pi,sqrt, exp,cos,sin,tan,argpartition,copy,trapz,mean,cov,vstack,hstack
+from numpy import log,pi,exp,cos,sin,tan,sqrt
 import numpy as np
 import random
 from scipy.special import erfinv
@@ -9,7 +9,6 @@ random.seed(2143178)
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--redshift", help="the redshift of the galaxy mock", type=float, default=None, required=True,)
-    parser.add_argument("--Om", help="the Omega_m of the galaxy mock", type=float, default=None, required=True)
     parser.add_argument("--size", help="the length of the galaxy mock", type=int, default=None, required=True)
     parser.add_argument("--source", help="systematics measurements comes from which survey? SDSS or DESI or any. If you chose any, you need to specify the numbers (see the tutorial ipynb)", type=str, default='DESI', required=None, choices=['DESI','SDSS','any'])
     parser.add_argument("--geometry", help="are galaxies in box or survey-like geometry? box or survey", type=str, default='box', required=None, choices=['box','survey'])
@@ -23,25 +22,21 @@ def parse_args():
 args = parse_args()
 
 z       = args.redshift
-Om      = args.Om
 size    = args.size
 source  = args.source
 geometry= args.geometry
 tracer  = args.tracer
 output  = args.output
 
-# assume LCDM without curvature
-Ode = 1-Om
-H   = 100*np.sqrt(Om*(1+z)**3+Ode)
-
 # we use inverse transform sampling to generate random numbers with Gaussian, Lorentzian and lognormal profiles from uniform random numbers
 def inv_trans(uniform, func='G', sigma=None):
     if func == 'G': # Gaussian profile
-        invfunc = sigma*sqrt(-2*log(uniform))*cos(2*pi*uniform)
+        half    = int(len(uniform)/2)
+        invfunc = np.append(sigma*sqrt(-2*log(uniform[:half]))*cos(2*pi*uniform[half:]),sigma*sqrt(-2*log(uniform[:half]))*sin(2*pi*uniform[half:]))
     elif func == 'L': # Lorentzian profile
         invfunc = tan((uniform-1/2)*pi)*sigma
-    elif func == 'lnG':
-        invfunc = 3321/500-np.exp((erfinv(1-200/99*uni)*243*np.sqrt(2)+657)/1000)
+    elif func == 'lnG': # lognormal profile
+        invfunc = 3321/500-exp((erfinv(1-200/99*uniform)*243*np.sqrt(2)+657)/1000)
     return invfunc   
 
 # the actual specroscopic systematics implementation:
@@ -51,26 +46,27 @@ def spec_systematics(uniform, func='L', sigma=None, maxdv=None, dvcatas=1000, dv
     if   func == 'L':
         outliers     = abs(Delta_v)>maxdv
         while np.sum(outliers)>0:
-            Delta_v[outliers] =  inv_trans(np.random.rand(np.sum(outliers))) 
+            Delta_v[outliers] =  inv_trans(np.random.rand(np.sum(outliers)), func=func, sigma=sigma) 
             outliers = abs(Delta_v)>maxdv
     elif func == 'lnG':
         dv_tot       = Delta_v.copy()
         outliers     = (dv_tot<np.log10(dvcatas))|(dv_tot>np.log10(dvcatasmax))|(~np.isfinite(dv_tot))
         while np.sum(outliers)>0:
-            dv_tot[outliers] = inv_trans(np.random.rand(np.sum(outliers))) 
+            dv_tot[outliers] = inv_trans(np.random.rand(np.sum(outliers)), func=func, sigma=sigma) 
             outliers = (dv_tot<np.log10(dvcatas))|(dv_tot>np.log10(dvcatasmax))|(~np.isfinite(dv_tot))  
         Delta_v      = 10**np.append(dv_tot[:int(len(dv_tot)/2)],-dv_tot[int(len(dv_tot)/2):])
         random.shuffle(Delta_v)
     return Delta_v
 
 # generate the uniform random numbers
+if size%2 ==1: size = size -1 
 uniform  = np.random.rand(size)
 # Implement the box
 if geometry == 'box':
     ## for LRGs
     if tracer == 'LRG':
         print('only redshift uncertainty vsmearing on peculiar velocity will be implemented')
-        if zerr is not None:
+        if args.zerr is not None:
             ### any redshift uncertainty you specify
             dv    = spec_systematics(uniform, func='G', sigma=args.zerr)
             if (args.zerr<20)|(args.zerr>90): warnings.warn('LRG Gaussian redshift uncertainty at 0.2<z<1.1 should be 20<zerr<90 km/s')
@@ -98,7 +94,7 @@ if geometry == 'box':
             elif source == 'DESI':
                 # implement redshift uncertainty
                 if   (0.8<z)&(z<=1.1):   vsmear = spec_systematics(uniform, func='L', sigma=9.3, maxdv=400)
-                elif (1.1<z)&(z<=1.6):   vsmear = spec_systematics(uniform, func='L', sigma=13.4, maxdv=400)
+                elif (1.1<z)&(z<=1.6):   vsmear = spec_systematics(uniform, func='L', sigma=13.4,maxdv=400)
                 elif (z<0.8)|(z>1.6): raise ValueError("redshift for DESI ELG-like mocks should be at 0.8<z<1.6")
                 # implement catastrophics 
                 vcatas  = np.zeros(size)
@@ -119,7 +115,7 @@ if geometry == 'box':
         else: raise ValueError("Provide zerr and fcatas simulteneously for ELGs")
     elif tracer == 'QSO':
         print('only redshift uncertainty+velocity shift vsmearing on peculiar velocity will be implemented')
-        if zerr is not None:
+        if args.zerr is not None:
             ### any redshift uncertainty you specify
             dv    = spec_systematics(uniform, func='G', sigma=args.zerr)
             if (args.zerr<80)|(args.zerr>300): warnings.warn('QSO Lorentzian redshift uncertainty+velocity shift at 0.8<z<2.1 should be 80<zerr<300 km/s')
@@ -131,7 +127,7 @@ if geometry == 'box':
                 elif (1.1<z)&(z<=1.6):   dv = spec_systematics(uniform, func='L', sigma=78, maxdv=2000)
                 elif (1.6<z)&(z<=2.1):   dv = spec_systematics(uniform, func='L', sigma=273, maxdv=2000)
                 elif (z<0.8)|(z>2.1): raise ValueError("redshift for DESI QSO-like mocks should be at 0.8<z<2.1")
-    np.savetxt(output,dv,header='Delta-v, add it to the peculiar velocity of your galaxy mocks')
+    np.savetxt(output,dv,header='Delta-v (km/s), add it to the peculiar velocity of your galaxy mocks')
 else:
     if source == 'SDSS':    raise ValueError("SDSS-like spectroscopic systematics in lightcones are not ready")
     elif source == 'DESI':  print('DESI-like spectroscopic systematics (including WEIGHT_FKP recomputing) are implemented in the subdirectory ./DESI_lighcone')
